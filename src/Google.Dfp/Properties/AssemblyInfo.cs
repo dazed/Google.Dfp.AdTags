a3a082cf6a1aa241740abb26fbc9d93faba30785
LukeSpice <lukespice@mail.com>
2014-10-23 17:35:48 +0100

    Added nuspec file

--- src/Google.Dfp/Properties/AssemblyInfo.cs
+++ src/Google.Dfp/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Google.Dfp")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("For easy use of Google DoubleClick for publisher Ad Tags in an .NET Web Application.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Dazed")]
 [assembly: AssemblyProduct("Google.Dfp")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
